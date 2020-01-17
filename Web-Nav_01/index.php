6932367a4454574363b1d7b8356d28516d2bd319
russianJoker <36176922+russianJoker@users.noreply.github.com>
2020-01-16 19:33:06 -0500

    Add files via upload

---
+++ Web-Nav_01/index.php
@@ -0,0 +1,27 @@
+<?php # Script 3.7 - index.php #2
+
+// This function outputs theoretical HTML
+// for adding ads to a Web page.
+ // End of the function definition.
+
+$page_title = 'Welcome to this Site!';
+include ('includes/header.html');
+// Call the function:
+
+?>
+
+<h1>Step-by-Step</h1>
+     <p> I am experimenting with multiple html and php files, using include() php function and looking how to best organize files</p>
+	<p>
+	Using css which I am still struggling to understand its posible to create stunning front-end user experience. I am more interested in backend data processing so I probably wont spend too much of my time playing with colors and stuff. Lets see what else is possible...
+	p.s. Siemense website is awesome ('I wanna ge hired') 
+	</p>
+
+
+
+<?php
+
+
+
+include ('includes/footer.html');
+?>
